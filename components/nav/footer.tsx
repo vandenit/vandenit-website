6dac0ed34adeced95c7c17607f6099e58cabe008
Filip Van den Broeck <fvb320@gmail.com>
2024-09-11 06:39:37 +0200

    footer fixes

--- components/nav/footer.tsx
+++ components/nav/footer.tsx
@@ -3,8 +3,7 @@ import React from "react";
 import Link from "next/link";
 import { Icon } from "../icon";
 import { useLayout } from "../layout/layout-context";
-import { RawRenderer } from "../raw-renderer";
-import { Container } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 
 export default function Footer() {
   const { theme, globalSettings, pageData } = useLayout();
@@ -13,26 +12,25 @@ export default function Footer() {
   return (
     <footer>
       <Container>
-        <div className="flex justify-between items-center gap-6 flex-wrap">
+        <Flex
+          justify="between"
+          align="center"
+          gap="6"
+          wrap="wrap"
+        >
           <Link
             href="/"
             className="group mx-2 flex items-center font-bold tracking-tight text-gray-400 dark:text-gray-300 opacity-50 hover:opacity-100 transition duration-150 ease-out whitespace-nowrap"
           >
             <Icon
-              parentColor={footer.color}
               data={{
                 name: globalSettings?.header.icon.name,
                 color:
-                  theme.color === "primary"
-                    ? "primary"
-                    : globalSettings?.header.icon.color,
-                style: globalSettings?.header.icon.style,
+                  footer.color,
               }}
-              className="inline-block h-10 w-auto group-hover:text-orange-500"
             />
           </Link>
-          <RawRenderer parentColor={footer.color} rawData={pageData} />
-        </div>
+        </Flex>
         <div />
       </Container>
     </footer>
